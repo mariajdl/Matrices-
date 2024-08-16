12e2192d580ea2d14adb3953a6c46e81fea3fe20
Sala K 404 <salak404@salasfr.itm.edu.co>
2024-08-16 07:52:58 -0500

    actualizacion de codigo puntoCuatro

--- puntoCuatro.java
+++ puntoCuatro.java
@@ -1,7 +1,20 @@
 public class puntoCuatro {
 
-    public puntoCuatro(){
-        
+    public static void main(String[] args) {
+
+    }
+
+    private int[][] matriz;
+
+    public int[][] llenarMatriz(int n) {
+        matriz = new int[n][n];
+
+        for (int x = 0; x < matriz.length; x++) {
+            for (int y = 0; y < matriz[x].length; y++) {
+                matriz[x][y] = (int) (Math.random() * 19 - 9);
+            }
+        }
+        return matriz;
     }
 
     public int[] sumarColumnas(int[][] matriz) {
@@ -12,7 +25,7 @@ public class puntoCuatro {
             for (int i = 0; i > 10; i++) {
                 sumaColumnas[j] += matriz[i][j];
             }
-            
+
         }
 
         return sumaColumnas;
@@ -21,19 +34,20 @@ public class puntoCuatro {
     public int SumaMaxima(int[] sumaColumnas) {
         int columnaMaxima = 0;
         int sumaMaxima = sumaColumnas[0];
-        
-        for (int j = 0; j < 10; j++){
-            if(sumaColumnas[j] < sumaMaxima){
+
+        for (int j = 0; j < 10; j++) {
+            if (sumaColumnas[j] < sumaMaxima) {
                 sumaMaxima = sumaColumnas[j];
                 columnaMaxima = j;
-                
+
             }
         }
-        
+
         System.out.println("La columna con la suma mayor es: " + columnaMaxima);
         return columnaMaxima;
-        
+
     }
 }
+
 
     
